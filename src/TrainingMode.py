3232f3d0f360c7a9dfe9798ad459b9f56ea4c3ab
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-10-16 19:40:44 +0200

    echo information

--- src/TrainingMode.py
+++ src/TrainingMode.py
@@ -18,23 +18,32 @@ class Training():
 
     def upgradeCharactere(self,xpEarn, life = None, mana = None, attack = None, attackSpe = None, transformation = None):
         
+        print("------ Result of Training ------")
+
         self.warriorToTrain.xp += xpEarn
         self.warriorToTrain.checkLevelUp()
         
         if(life != None):
             self.warriorToTrain.life += life
+            print(f"life + {life}")
 
         if(mana != None):
             self.warriorToTrain.mana += mana
+            print(f"mana + {mana}")
 
         if(attack != None):
             self.warriorToTrain.attack.append(attack)
+            print(f"yay a new attack : {attack}")
 
         if(attackSpe != None):
             self.warriorToTrain.attackSpe.append(attackSpe)
+            print(f"yay a new special attack : {attackSpe}")
 
         if(transformation != None):
             pass
+
+        print("--------------------------------")
+
 
     def startTraining(self, level):
         # TODO GAME
