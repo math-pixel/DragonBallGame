5d700ac5b03f8590865591a421ba058ae1efe421
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-10-16 14:03:43 +0200

    test attack

--- src/Attacks.py
+++ src/Attacks.py
@@ -1,19 +1,35 @@
 # --------- Attack General Protocol
 class AttackProtocol:
-    pass
+    
+    def __init__(self):
+        self.consumeMana = 3
+        self.dammage = 5
 
 # --------- All Attack Protocol
 class AttackStandard(AttackProtocol):
-    pass
+    def __init__(self):
+        super().__init__()
 
 class AttackSpe(AttackProtocol):
-    pass
+    def __init__(self):
+        super().__init__()
 
 # --------- Attacks
 class PunchAttack(AttackStandard):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.consumeMana = 0
+        self.dammage = 1
 
 
 # --------- Attack Spe
-class chargePunch(AttackSpe):
-    pass
+class PunchCharged(AttackSpe):
+    def __init__(self):
+        super().__init__()
+        self.consumeMana = 1
+        self.dammage = 3
+
+if __name__ == "__main__":
+    punch = PunchAttack()
+    chargedPunch = PunchCharged()
+    print(punch.dammage, " - ", chargedPunch.dammage)
